4fed88d66b597381a5d640bbeb82d516c88117f6
gwoz <mikegwozdek@gmail.com>
2016-07-16 18:26:45 -0400

    Add iPhone displays for apps and update structure

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
+  
+  // Fullpage.js Settings
+
   $('#fullpage').fullpage({
-    anchors:['main', 'fintech', 'projects'],
+    anchors:['main', 'development', 'consulting'],
     continuousVertical: true,
-    sectionsColor : ['#78CAD2', '#52B788', '#FFF07C']
+    sectionsColor : ['#78CAD2', '#FFF07C', '#52B788']
+  });
+
+  // iPhone Previews
+
+  bioMp(document.getElementById('battleground-preview'), {
+    url: 'https://github.com/gwoz/battleground',
+    view: 'front',
+    image: '../images/iphone6_front_black.png',
+    scale: 0.4
+  });
+
+  bioMp(document.getElementById('meteor404-preview'), {
+    url: 'http://meteor404.herokuapp.com',
+    view: 'front',
+    image: '../images/iphone6_front_black.png',
+    scale: 0.4
   });
 });
