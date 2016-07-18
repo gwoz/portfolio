962095a387379c137dfb33ac47ff539f54e58fde
gwoz <mikegwozdek@gmail.com>
2016-07-18 08:38:00 -0400

    Initial commit for pages

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  
+  // Fullpage.js Settings
+
+  $('#fullpage').fullpage({
+    anchors:['main', 'development', 'consulting'],
+    continuousVertical: true
+  });
+
+  // iPhone Previews
+
+  // bioMp(document.getElementById('battleground-preview'), {
+  //   url: 'https://github.com/gwoz/battleground',
+  //   view: 'front',
+  //   image: '../images/iphone6_front_black.png',
+  //   scale: 0.4
+  // });
+
+  // bioMp(document.getElementById('meteor404-preview'), {
+  //   url: 'http://meteor404.herokuapp.com',
+  //   view: 'front',
+  //   image: '../images/iphone6_front_black.png',
+  //   scale: 0.4
+  // });
+});
